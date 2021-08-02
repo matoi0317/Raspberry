6f44bb9fd54cc51d078a80600c226924c17adddf
matoi0317 <matoi.shibanuma@gmail.com>
2021-08-02 18:25:46 +0900

    sensor2

---
+++ sensor2.py
@@ -0,0 +1,21 @@
+from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+import time
+
+# HC SR04の使用するピン
+PIN_TRIG = 22
+PIN_ECHO = 27
+
+def main():
+    # ピン設定
+    factory = PiGPIOFactory()
+    sensor = DistanceSensor(PIN_ECHO, PIN_TRIG, pin_factory=factory)
+
+    for _ in range(100):
+        print('Distance to nearest object is', sensor.distance, 'm')
+        time.sleep(1)
+    # print("distance:", distance, "cm")
+
+if __name__ == "__main__":
+    main()
