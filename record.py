ff8117a9fbb76b5191d60527009a161c9329282d
matoi0317 <matoi.shibanuma@gmail.com>
2021-08-07 15:40:37 +0900

    r11

--- record.py
+++ record.py
@@ -1,8 +1,10 @@
 import subprocess
+import time
 import speech_recognition as sr
 a1 = 'arecord -D plughw:0,0 -d 10 -f cd test3.wav'
 proc = subprocess.Popen(a1, shell=True)
-proc.sleep(5)
+time.sleep(5)
+proc.kill()
 r = sr.Recognizer()
 with sr.AudioFile("test3.wav") as source:
     audio = r.record(source)
