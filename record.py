79dab18c4c8f55513fce0398fe45a469a70450b8
matoi0317 <matoi.shibanuma@gmail.com>
2021-08-07 15:10:16 +0900

    record3

--- record.py
+++ record.py
@@ -1,7 +1,7 @@
 import subprocess
 import speech_recognition as sr
 
-a1 = 'arecord -D plughw:0,0 -d 10 -f cd tes3.wav'
+a1 = 'arecord -D plughw:0,0 -d 10 -f cd test3.wav'
 proc = subprocess.Popen(a1, shell=True)
 proc.wait(11)
 r = sr.Recognizer()
