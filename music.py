f4fbe215f5bd718680dae56b0484fba6c1a0265f
matoi0317 <matoi.shibanuma@gmail.com>
2021-09-04 14:12:21 +0900

    time

--- music.py
+++ music.py
@@ -1,3 +1,9 @@
+import datetime
+import time
 import subprocess
+time = "15"
 a1 = 'mpg123 -a hw:2,0 summer2.mp3'
-proc = subprocess.run(a1, shell=True)
+n = datetime.datetime.now()
+nm = n.minute
+if time == nm:
+    proc = subprocess.run(a1, shell=True)
