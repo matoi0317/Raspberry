244223c3c8e801cbb6929ef55827c211eead72c0
matoi0317 <matoi.shibanuma@gmail.com>
2021-09-04 14:28:10 +0900

    music3

--- music.py
+++ music.py
@@ -3,14 +3,15 @@
 import datetime
 import time
 import subprocess
-time = 24
+time = 29
 i = 1
 a1 = 'mpg123 -a hw:2,0 summer2.mp3'
+a2 = 'arecord -D plughw:0,0 -d 10 -f cd test3.wav'
 while i < 2:
     n = datetime.datetime.now()
     nm = n.minute
     if time == nm:
         proc = subprocess.run(a1, shell=True)
         print("音声を停止し、録音を開始します。")
-        a1 = 'arecord -D plughw:0,0 -d 10 -f cd test3.wav'
+        proc = subprocess.run(a2, shell=True)
         i += 1
