9e19489a210d364f2717af75740ef4fc43a0de3e
matoi0317 <matoi.shibanuma@gmail.com>
2021-09-11 14:17:32 +0900

    ccc

---
+++ aaaaaa.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import mm
+from reportlab.pdfbase.pdfmetrics import registerFont
+from reportlab.pdfbase.ttfonts import TTFont
+
+registerFont(TTFont('GenShinGothic',
+                    './data/GenShinGothic-Monospace-Medium.ttf'))
+
+file = 'reportlab-test.pdf'  # 出力ファイル名を設定
+
+paper = canvas.Canvas(file)  # 白紙のキャンバスを用意
+paper.saveState()  # 初期化
+paper.setFont('GenShinGothic', 24)  # フォントを設定
+
+# 横wと縦hの用紙サイズを設定
+w = 210 * mm
+h = 297 * mm
+
+paper.setPageSize((w, h))  # 用紙のサイズをセット
+paper.drawString(10, h - 30,  # テキストの書き込み
+                 'ご飯を食べたか')
+paper.drawString(10, h - 60,  # テキストの書き込み
+                 '起きた時間')
+paper.save()
