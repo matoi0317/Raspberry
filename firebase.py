2d21d7fb7da31855213201ed10265928371e4503
matoi0317 <matoi.shibanuma@gmail.com>
2021-09-13 18:42:33 +0900

    ggggggg

--- firebase.py
+++ firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore, storage
 
 cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.json')
-firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com/'})
+firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
 
 bucket = storage.bucket()
 
