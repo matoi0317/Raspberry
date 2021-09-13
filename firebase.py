20039152041dae2ab3159d5b72fa2ffdc6174857
matoi0317 <matoi.shibanuma@gmail.com>
2021-09-13 17:49:01 +0900

    firebase

---
+++ firebase.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore, storage
+
+cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.json')
+firebase_admin.initialize_app(cred, {'storageBucket': 'test'})
+
+bucket = storage.bucket()
+
+filename = 'eto_tora_banzai.png'
+content_type = 'image/png'
+blob = bucket.blob(filename)
+with open(filename, 'rb') as f:
+    blob.upload_from_file(f, content_type=content_type)
