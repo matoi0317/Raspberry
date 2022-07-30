41d29e6a0d037d0657ef0ccc7576c9c8942106de
matoi0317 <matoi.shibanuma@gmail.com>
2022-07-30 15:31:53 +0900

    temp5

--- temp.py
+++ temp.py
@@ -19,7 +19,7 @@ for k in range(1):
     sensordata = sensor.pixels
     # for i in sensordata:
     #     print(max(i))
-print(sum(sensordata)/8)
+print(sum(sensordata))
     #
     # # bicubicのデータ
     # fig = plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
