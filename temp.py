e23969eb70d536d49c6e92c65b875478429bd704
matoi0317 <matoi.shibanuma@gmail.com>
2022-07-30 15:26:04 +0900

    temp2

--- temp.py
+++ temp.py
@@ -17,7 +17,7 @@ time.sleep(.1)
 while True:
     # データ取得
     sensordata = sensor.pixels
-    print(sensordata)
+    print(max(sensordata))
     #
     # # bicubicのデータ
     # fig = plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
