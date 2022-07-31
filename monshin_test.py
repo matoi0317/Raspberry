e742d02d2b25e67c804a596ae8f7e7d3f9b705da
matoi0317 <matoi.shibanuma@gmail.com>
2022-07-31 22:06:41 +0900

    type

--- monshin_test.py
+++ monshin_test.py
@@ -9,7 +9,7 @@ user_id = args[1]
 cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.json')
 firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
 db = firestore.client()
-docs = db.collection("users").where("id","==",user_id).get()
+docs = db.collection("users").where("id","==",int(user_id)).get()
 for doc in docs:
     print(doc.to_dict())
 
