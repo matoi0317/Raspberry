30f5076fd9e08455133ab19fc4d2565139b6fc04
matoi0317 <matoi.shibanuma@gmail.com>
2022-07-31 21:49:14 +0900

    args2

--- monshin_test.py
+++ monshin_test.py
@@ -4,10 +4,10 @@ from firebase_admin import credentials
 from firebase_admin import firestore, storage
 import sys
 
-cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.json')
-firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
-db = firestore.client()
-docs = db.collection("users").get()
+# cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.json')
+# firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
+# db = firestore.client()
+# docs = db.collection("users").get()
 args = sys.argv
 print(args[1])
 # # coding: utf-8
