13942a64bb150def7ebd4b5e69e0792cb4221582
matoi0317 <matoi.shibanuma@gmail.com>
2022-08-02 06:26:52 +0900

    short

--- monshin_test.py
+++ monshin_test.py
@@ -30,58 +30,62 @@ cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.
 firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
 db = firestore.client()
 docs = db.collection("users").where("id","==",int(user_id)).get()
-while i < 2:
-    n = datetime.datetime.now()
-    nm = n.minute
-    if mmm == nm:
-        #問診の時間です
-        proc = subprocess.run(a5, shell=True)
-        for doc in docs:
-            if doc.to_dict()["q1"] == True:
-                proc = subprocess.run(a1, shell=True)
-                proc = subprocess.run(a2, shell=True)
-                time.sleep(0)
-            else:
-                print("False")
-            if doc.to_dict()["q2"] == True:
-                proc = subprocess.run(a3, shell=True)
-                # 録音
-                proc = subprocess.run(a4, shell=True)
-                time.sleep(0)
-            else:
-                print("false")
-            if doc.to_dict()["q3"] == True:
-                proc = subprocess.run(a6, shell=True)
-                # 録音
-                proc = subprocess.run(a7, shell=True)
-                time.sleep(0)
-            else:
-                print("false")
-            if doc.to_dict()["q4"] == True:
-                proc = subprocess.run(a8, shell=True)
-                # 録音
-                proc = subprocess.run(a9, shell=True)
-                time.sleep(0)
-            else:
-                print("false")
-        i += 1
-
-
-
-r = sr.Recognizer()
-with sr.AudioFile("food.wav") as source:
-    audio = r.record(source)
-text = r.recognize_google(audio, language="ja-JP")
-print(text)
-with sr.AudioFile("wakeup.wav") as source:
-    audio2 = r.record(source)
-text2 = r.recognize_google(audio2, language="ja-JP")
-print(text2)
-with sr.AudioFile("medicine.wav") as source:
-    audio3 = r.record(source)
-text3 = r.recognize_google(audio3, language="ja-JP")
-with sr.AudioFile("syoujou.wav") as source:
-    audio4 = r.record(source)
-text4 = r.recognize_google(audio4, language="ja-JP")
+# while i < 2:
+#     n = datetime.datetime.now()
+#     nm = n.minute
+#     if mmm == nm:
+#         #問診の時間です
+#         proc = subprocess.run(a5, shell=True)
+#         for doc in docs:
+#             if doc.to_dict()["q1"] == True:
+#                 proc = subprocess.run(a1, shell=True)
+#                 proc = subprocess.run(a2, shell=True)
+#                 time.sleep(0)
+#             else:
+#                 print("False")
+#             if doc.to_dict()["q2"] == True:
+#                 proc = subprocess.run(a3, shell=True)
+#                 # 録音
+#                 proc = subprocess.run(a4, shell=True)
+#                 time.sleep(0)
+#             else:
+#                 print("false")
+#             if doc.to_dict()["q3"] == True:
+#                 proc = subprocess.run(a6, shell=True)
+#                 # 録音
+#                 proc = subprocess.run(a7, shell=True)
+#                 time.sleep(0)
+#             else:
+#                 print("false")
+#             if doc.to_dict()["q4"] == True:
+#                 proc = subprocess.run(a8, shell=True)
+#                 # 録音
+#                 proc = subprocess.run(a9, shell=True)
+#                 time.sleep(0)
+#             else:
+#                 print("false")
+#         i += 1
+#
+#
+#
+# r = sr.Recognizer()
+# with sr.AudioFile("food.wav") as source:
+#     audio = r.record(source)
+# text = r.recognize_google(audio, language="ja-JP")
+# print(text)
+# with sr.AudioFile("wakeup.wav") as source:
+#     audio2 = r.record(source)
+# text2 = r.recognize_google(audio2, language="ja-JP")
+# print(text2)
+# with sr.AudioFile("medicine.wav") as source:
+#     audio3 = r.record(source)
+# text3 = r.recognize_google(audio3, language="ja-JP")
+# with sr.AudioFile("syoujou.wav") as source:
+#     audio4 = r.record(source)
+# text4 = r.recognize_google(audio4, language="ja-JP")
+text = "はい"
+text2 = "いいえ"
+text3 = "ok"
+text4 = "no"
 Create_pdf(text, text2,text3,text4,int(user_id))
 
