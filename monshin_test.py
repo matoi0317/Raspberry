ded2d4bb3d8bf93f76fa0e895636d688976ead39
matoi0317 <matoi.shibanuma@gmail.com>
2022-08-01 19:34:35 +0900

    2kaime

--- monshin_test.py
+++ monshin_test.py
@@ -10,6 +10,8 @@ import datetime
 
 a1 = 'mpg123 -a hw:2,0 food2.mp3'
 a2 = 'arecord -D plughw:0,0 -d 10 -f cd food.wav'
+a3 = "mpg123 -a hw:0,0 wakeup2.mp3"
+a4 = 'arecord -D plughw:2,0 -d 10 -f cd wakeup.wav'
 a5 = 'mpg123 -a hw:2,0 monshin2.mp3'
 
 mmm = int(input())
@@ -20,23 +22,36 @@ cred = credentials.Certificate('karute-81f3c-firebase-adminsdk-na7p6-099144bd72.
 firebase_admin.initialize_app(cred, {'storageBucket': 'karute-81f3c.appspot.com'})
 db = firestore.client()
 docs = db.collection("users").where("id","==",int(user_id)).get()
-for doc in docs:
-    if doc.to_dict()["q1"] == True:
-        while i < 2:
-            n = datetime.datetime.now()
-            nm = n.minute
-            if mmm == nm:
-                # 問診の時間です
-                proc = subprocess.run(a5, shell=True)
-                # ご飯は食べましたか？
+while i < 2:
+    n = datetime.datetime.now()
+    nm = n.minute
+    if mmm == nm:
+        #問診の時間です
+        proc = subprocess.run(a5, shell=True)
+        for doc in docs:
+            if doc.to_dict()["q1"] == True:
                 proc = subprocess.run(a1, shell=True)
-                # 録音
                 proc = subprocess.run(a2, shell=True)
                 time.sleep(0)
-                i += 1
+            else:
+                print("False")
+            if doc.to_dict()["q2"] == True:
+                proc = subprocess.run(a3, shell=True)
+                # 録音
+                proc = subprocess.run(a4, shell=True)
+                time.sleep(0)
+            else:
+                print("false")
+        i += 1
+
+
 
 r = sr.Recognizer()
 with sr.AudioFile("food.wav") as source:
     audio = r.record(source)
 text = r.recognize_google(audio, language="ja-JP")
 print(text)
+with sr.AudioFile("wakeup.wav") as source:
+    audio2 = r.record(source)
+text2 = r.recognize_google(audio2, language="ja-JP")
+print(text2)
